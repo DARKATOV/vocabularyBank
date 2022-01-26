45f54787917fb7b3f9b74b2d75ce14dd4b63599f
DARKATOV <76508674+DARKATOV@users.noreply.github.com>
2022-01-26 03:41:12 +0100

    act code

---
+++ englishVocabulary.js
@@ -0,0 +1,10 @@
+const vocabulary = [
+"spread",
+"knowledge",
+"ledge",
+"sense",
+"word",
+"sentence"
+];
+const wordSelected = vocabulary[Math.floor(Math.random()*vocabulary.length)]
+console.log(wordSelected);
