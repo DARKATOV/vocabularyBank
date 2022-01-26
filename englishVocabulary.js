086a79490b303d96faee9f7231c11e14519d8411
DARKATOV <76508674+DARKATOV@users.noreply.github.com>
2022-01-26 05:04:10 +0100

    v.1

--- englishVocabulary.js
+++ englishVocabulary.js
@@ -1,10 +1,45 @@
-const vocabulary = [
-"spread",
-"knowledge",
-"ledge",
-"sense",
-"word",
-"sentence"
-];
-const wordSelected = vocabulary[Math.floor(Math.random()*vocabulary.length)]
-console.log(wordSelected);
+const macroVocabulary = {
+    englishVocabulary : [
+        "spread",
+        "knowledge",
+        "ledge",
+        "sense",
+        "word",
+        "sentence",
+        "windows",
+        "lenght",
+        "index",
+        "amount",
+        "string"
+    ],
+    unixVocabulary : [
+        "pwd",
+        "ls",
+        "cd",
+        "mkdir",
+        "touch",
+        "cat >",
+        "rm",
+        "rm -r",
+        "grep"
+    ],
+    computingVocabulary : [
+        "solaris",
+        "sun",
+        "bit",
+        "byte",
+    ]
+};
+function engine (vocabulariesData){
+    const numberOfVocabularies = Object.keys(vocabulariesData).length;
+    for (let index = 0; index < numberOfVocabularies; index++) {
+        const vocabularies = Object.keys(vocabulariesData);
+        const selectedVocabulary = vocabularies[index];
+        let stringVocabulary = selectedVocabulary.toString();
+        const vocabulary = macroVocabulary[stringVocabulary];
+        typeof vocabulary;
+        const selectedWord = vocabulary[Math.floor((Math.random()*vocabulary.length))];
+        console.log(selectedWord);
+    }
+};
+console.log(engine(macroVocabulary));
